36ae277264ce38a31b7ec050144ef74fb599311e
psteeep <psteeep@users.noreply.github.com>
2020-11-09 13:31:36 +0200

    not all

--- last_one.cpp
+++ last_one.cpp
@@ -4,28 +4,42 @@
 using namespace std;
 
 class Parent {
+private:
+	int *friends;
+
+protected:
+	int age;
+
+public:
+	
 	Parent() {
-
-
+		age = 40;
+		friends = new int[100];
 	}
 
 	~Parent() {
-
-
+		delete[] friends;
 	}
 
-
+	int getAge() {
+		return age;
+	}
 };
 
 
 class Child : public Parent {
-
-
+public:
+	Child() {
+		age = 14;
+	}
 
 };
 
 
 int main() {
+	
+	Parent first;
+	Child sec;
 
 
 
