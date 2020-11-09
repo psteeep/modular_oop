ef44581d0b6437950aac62f229e63475771dd423
psteeep <psteeep@users.noreply.github.com>
2020-11-09 13:04:30 +0200

    Enter numbers only

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 
 int main() {
+
+	int numb;
+
+	vector<int> list0;
+
+	while (true) {
+
+		cout << "Enter number (0 == break) => ";
+		cin >> numb;
+
+		if (numb != 0) {
+			list0.push_back(numb);
+		}
+	}
 
 
 
