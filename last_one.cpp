3e97a7d13f317b0cc65885e0032d86986d2df5f3
psteeep <psteeep@users.noreply.github.com>
2020-11-09 13:21:32 +0200

    Add last one tasck with class

---
+++ last_one.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+using namespace std;
+
+class Parent {
+	Parent() {
+
+
+	}
+
+	~Parent() {
+
+
+	}
+
+
+};
+
+
+class Child : public Parent {
+
+
+
+};
+
+
+int main() {
+
+
+
+	return 0;
+}
